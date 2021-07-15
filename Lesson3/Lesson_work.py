6029707d7f6a9a98be517e98b2dd22afeca8a58a
AnnTka <tovliani.mze@gmail.com>
2021-07-16 00:45:16 +0300

    Class Work

---
+++ Lesson3/Lesson_work.py
@@ -0,0 +1,40 @@
+x = 17 / 2 * 3 + 2
+print(x)
+x = 2 + 17 / 2 * 3
+print(x)
+x = 19 % 4 + 15 / 2 * 3
+print(x)
+x = (15 + 6) - 10 * 4
+print(x)
+x = 17 / 2 % 2 * 3
+print(x)
+
+x = (17 / 2) * 3 + 2
+print(x)
+x = 2 + (17 / 2 * 3)
+print(x)
+x = (19 % 4) + (15 / 2 * 3)
+print(x)
+x = (15 + 6) - (10 * 4)
+print(x)
+x = (17 / 2) % 2 * 3
+print(x)
+
+dima = 21
+jane = 23
+vova = 18
+print(dima + vova + jane)
+
+average = (dima + vova + jane) / 3
+print(average)
+
+side_square = 10
+new_list = [side_square * 4, side_square * side_square, (side_square ** 2 + side_square ** 2) ** 0.5]
+print(new_list)
+
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+print((len(my_list)) - len(set(my_list)))
+
+list_my = my_list[1:4]
+list_my.reverse()
+print(list_my)
