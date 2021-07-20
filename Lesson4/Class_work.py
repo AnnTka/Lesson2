7335bbc5824098a68906fe93bc7636bc9a20710a
AnnTka <tovliani.mze@gmail.com>
2021-07-20 23:33:23 +0300

    Add homework files

---
+++ Lesson4/Class_work.py
@@ -0,0 +1,80 @@
+# ПРИМЕР:
+# import random
+
+# my_list = []
+
+# Сгенерили список:
+# for _ in range(10):
+#     my_list.append(random.randint(0, 100))
+
+# А теперь проверяем больше ли оно 10 и добавляет его:
+# my_sum = 0
+# for element in my_list:
+#     if element > 10:
+#         my_sum = my_sum + element
+
+# print(my_sum)
+
+
+# Задание 1:
+# list = [5, 15, 1, 20]
+
+# sum = 0
+# for element in list:
+#     if element > 10:
+#         sum = sum + element
+
+# print(sum)
+
+
+# Задание 2:
+
+# Вводим число с клавиатуры и преобразовываем в целочисленное значение:
+# num = int(input())
+
+# Генератор чисел от 1 до 101:
+# for x in range(1, 101):
+
+# Выбираем числа, которые кратны тому, что мы ввели, и выводим:
+#     if x % num == 0:
+#        print(x)
+
+# Задание 3:
+
+# print("Введите число: ")
+# my_summ = int(input())
+
+# Сумма кубов всех чисел от 1 до введенного (включительно):
+# counter = 0
+# for element in range(1, my_summ + 1):
+#     counter += element ** 3
+
+# print(counter)
+
+# Задание 4:
+
+# import random
+
+# while True:
+#     my_num = random.randint(1, 10)
+
+#     if my_num == 7:
+#         break
+
+#     print(my_num)
+
+# Задание 5:
+
+number_1 = int(input())
+number_2 = int(input())
+
+counter = 0
+for element in range(number_1, number_2 + 1):
+    counter += element ** 3
+
+print(counter)
+
+# Задание 6: после среды
+
+
+# Задание 7: после среды
